aab544e0c2fcc037d250225ec8a60ec861e9fde0
igorekkkkkkk <goruk080808@gmail.com>
2025-02-08 19:06:12 +0100

    Delete strona.js

--- strona.js
+++
@@ -1,7 +0,0 @@
-$('body').on('click', '.password-checkbox', function(){
-	if ($(this).is(':checked')){
-		$('#pass').attr('type', 'text');
-	} else {
-		$('#pass').attr('type', 'password');
-	}
-}); 
